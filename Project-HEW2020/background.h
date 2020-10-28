0e5fb4261acc043da48071b540b577c86227eda0
CycleDM <cycledm0508@gmail.com>
2020-10-28 23:29:20 +0900

    背景制御モジュールを追加しました

---
+++ Project-HEW2020/background.h
@@ -0,0 +1,19 @@
+//----------------------------------------------------------------------------
+// 
+// Project-HEW2020 [background.h]
+// îwåiêßå‰ÉÇÉWÉÖÅ[Éã
+// 
+// Date:   2020/10/28
+// Author: AT12D187_17_é¸êi
+// 
+//----------------------------------------------------------------------------
+#pragma once
+
+#include "sprite.h"
+
+class SpriteBg : public SpriteNormal
+{
+public:
+	SpriteBg();
+	~SpriteBg();
+};
