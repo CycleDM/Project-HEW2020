16c824e5932e8bb954872b3eb31f89c15017c9a5
CycleDM <cycledm0508@gmail.com>
2020-11-12 03:45:59 +0900

    Update config.h

--- Project-HEW2020/config.h
+++ Project-HEW2020/config.h
@@ -20,6 +20,7 @@
 // テクスチャーのファイル名
 #define TEXTURE_DEBUG				"assets/texture/debug_font_32x64.png"
 #define TEXTURE_BG					"assets/texture/background_only.png"
+#define TEXTURE_BG_OVERLAY			"assets/texture/background_overlay.png"
 #define TEXTURE_PLAYER				"assets/texture/player.png"
 #define TEXTURE_ROBOT				"assets/texture/wurenji01.png"
 
