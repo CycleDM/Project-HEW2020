ceb84bae4cce1874d2ef32b4188ff7caa3f4de0f
CycleDM <cycledm0508@gmail.com>
2020-11-12 02:21:28 +0900

    Update player.cpp

--- Project-HEW2020/player.cpp
+++ Project-HEW2020/player.cpp
@@ -11,8 +11,6 @@
 #include "config.h"
 #include "player.h"
 #include "sprite.h"
-#include "object.h"
-#include "test_scene.h"
 #include "game.h"
 
 //-----------------------------------------------------------------------------
@@ -28,14 +26,22 @@ static Sprite* g_pSpritePlayer = NULL;
 static int g_nMoveFrame = 0;
 static int g_nMoveAnimeCnt = 0;
 
+GamePlayer::GamePlayer()
+{
+	this->Init();
+}
+GamePlayer::~GamePlayer()
+{
+	this->Uninit();
+}
+
 void GamePlayer::Init(void)
 {
 	pos = D3DXVECTOR2(64.0f, (float)SCREEN_HEIGHT);
 	dirc = D3DXVECTOR2(0.0f, 9.8f);
 	speed = 0.0f;
 	velocity = 5.0f;
-	hp = 3;
-	score = 0;
+	isMoving = false;
 	isJumping = false;
 	isClimbingUp = false;
 
@@ -59,15 +65,17 @@ void GamePlayer::Uninit(void)
 
 void GamePlayer::Update(void)
 {
-	// テクスチャの座標をプレイヤーの座標に更新
-	g_pSpritePlayer->SetDrawPos(pos.x - PLAYER_WIDTH / 2, pos.y - PLAYER_HEIGHT / 2);
 	// 進行方向を長さ１にする
 	//D3DXVec2Normalize(&dirc, &dirc);
 	// プレイヤー座標の更新（移動方向ｘ速度）
 	pos.x += dirc.x * speed;
 	speed *= 0.8f;
-	if (speed < 0.3f) speed = 0.0f;
-	//if (dirc.y < 9.8f && !isClimbingUp) dirc.y += 0.98f;
+	if (speed < 0.3f)
+	{
+		speed = 0.0f;
+		isMoving = false;
+	}
+	if (dirc.y < 9.8f && !isClimbingUp) dirc.y += 0.98f;
 	if (dirc.y > 9.8f) dirc.y = 9.8f;
 	pos.y += dirc.y;
 	// プレイヤー座標の修正
@@ -89,42 +97,6 @@ void GamePlayer::Update(void)
 		// 地面に戻ったらジャンプ中の状態から回復
 		this->isJumping = false;
 		this->isClimbingUp = false;
-	}
-	if (pos.y < (float)SCREEN_HEIGHT - 64.0f)
-	{
-		TestScene* scene = Game_GetScene();
-		GameObject* floor = scene->GetNearestFloor();
-		do
-		{
-			if (NULL == floor) break;
-
-			// Climb to floor
-			if (this->isClimbingUp && this->pos.y < floor->GetPosition().y)
-			{
-				this->pos.y = floor->GetPosition().y - 100.0f;
-			}
-			// Climb down
-			if (this->isClimbingDown)
-			{
-				break;
-			}
-			// Stand on floor
-			if (!this->isClimbingDown && floor->GetPosition().y - floor->GetPolygonHeight() <= pos.y + g_pSpritePlayer->GetPolygonHeight())
-			{
-				this->SetPosition(pos.x, 200.0f);
-			}
-			// Edge
-			if (this->pos.x < floor->GetPosition().x - 600.0f)
-			{
-				this->SetPosition(floor->GetPosition().x - 600.0f, pos.y);
-			}
-			if (this->pos.x > floor->GetPosition().x + 500.0f)
-			{
-				this->SetPosition(floor->GetPosition().x + 500.0f, pos.y);
-			}
-
-		} while (0);
-		
 	}
 
 	// Animation
@@ -151,6 +123,8 @@ void GamePlayer::Update(void)
 
 void GamePlayer::Draw(void)
 {
+	// テクスチャの座標をプレイヤーの座標に更新
+	g_pSpritePlayer->SetDrawPos(pos.x - PLAYER_WIDTH / 2, pos.y - PLAYER_HEIGHT / 2);
 	g_pSpritePlayer->Draw();
 }
 
@@ -175,16 +149,6 @@ float GamePlayer::GetPolygonHeight(void)
 	return g_pSpritePlayer->GetPolygonHeight();
 }
 
-int GamePlayer::GetHealth(void)
-{
-	return this->hp;
-}
-
-int GamePlayer::GetScore(void)
-{
-	return this->score;
-}
-
 float GamePlayer::GetSpeed(void)
 {
 	return this->speed;
@@ -195,8 +159,14 @@ D3DXVECTOR2 GamePlayer::GetDirection(void)
 	return this->dirc;
 }
 
+bool GamePlayer::GetMovingStatus(void)
+{
+	return this->isMoving;
+}
+
 void GamePlayer::MoveLeft(void)
 {
+	isMoving = true;
 	dirc.x = -1.0f;
 	speed = velocity;
 	g_pSpritePlayer->SetHorizontalFlip(true);
@@ -204,29 +174,10 @@ void GamePlayer::MoveLeft(void)
 
 void GamePlayer::MoveRight(void)
 {
+	isMoving = true;
 	dirc.x = 1.0f;
 	speed = velocity;
 	g_pSpritePlayer->SetHorizontalFlip(false);
-}
-
-void GamePlayer::ClimbUp(void)
-{
-	dirc.y = -5.0f;
-}
-
-void GamePlayer::ClimbDown(void)
-{
-	dirc.y = 5.0f;
-}
-
-void GamePlayer::SetClimbUpStatus(bool isClimbing)
-{
-	this->isClimbingUp = isClimbing;
-}
-
-void GamePlayer::SetClimbDownStatus(bool isClimbing)
-{
-	this->isClimbingDown = isClimbing;
 }
 
 void GamePlayer::Jump(void)
