731249816ada8d516561332968d5af68632183ee
CycleDM <cycledm0508@gmail.com>
2020-11-12 02:21:30 +0900

    Update player.h

--- Project-HEW2020/player.h
+++ Project-HEW2020/player.h
@@ -14,14 +14,8 @@
 class GamePlayer
 {
 public:
-	GamePlayer()
-	{
-		this->Init();
-	}
-	~GamePlayer()
-	{
-		this->Uninit();
-	}
+	GamePlayer();
+	~GamePlayer();
 
 	void Init(void);
 	void Update(void);
@@ -33,20 +27,13 @@ public:
 	D3DXVECTOR2 GetPosition(void);
 	float GetPolygonWidth(void);
 	float GetPolygonHeight(void);
-	// プレイヤーの体力を取得
-	int GetHealth(void);
-	// プレイヤーのスコアを取得
-	int GetScore(void);
 
 	float GetSpeed(void);
 	D3DXVECTOR2 GetDirection(void);
+	bool GetMovingStatus(void);
 
 	void MoveLeft(void);
 	void MoveRight(void);
-	void ClimbUp(void);
-	void ClimbDown(void);
-	void SetClimbUpStatus(bool);
-	void SetClimbDownStatus(bool);
 	void Jump(void);
 
 private:
@@ -54,9 +41,9 @@ private:
 	D3DXVECTOR2 dirc;	// 方向
 	float speed;		// 速度
 	float velocity;		// 加速度
-	int hp;				// 体力
-	int score;			// スコア
-	bool isJumping;		// ジャンプ中の判定
+
+	bool isMoving;			// 移動中の判定
+	bool isJumping;			// ジャンプ中の判定
 	bool isClimbingUp;
 	bool isClimbingDown;
 };
