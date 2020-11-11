4e7a660ab0bd04dff8038f50e7ec6939e6b62a3d
CycleDM <cycledm0508@gmail.com>
2020-11-12 02:15:39 +0900

    Update config.h

--- Project-HEW2020/config.h
+++ Project-HEW2020/config.h
@@ -9,14 +9,20 @@
 //----------------------------------------------------------------------------
 #pragma once
 
+// スクリーンサイズ
 #define SCREEN_WIDTH (1280)
 #define SCREEN_HEIGHT (720)
 
+// ゲームオブジェクトの最大数
+#define SINGLE_SCENE_OBJECT_MAX		(128)
+#define SINGLE_SCENE_OVERLAY_MAX	(4)
+
 // テクスチャーのファイル名
-#define TEXTURE_DEBUG		"assets/texture/debug_font_32x64.png"
-#define TEXTURE_BG			"assets/texture/background_only.png"
-#define TEXTURE_PLAYER		"assets/texture/player.png"
-#define TEXTURE_ROBOT		"assets/texture/wurenji01.png"
-#define TEXTURE_2FGROUND	"assets/texture/2floor.png"
-#define TEXTURE_LADDER		"assets/texture/ladder.png"
-#define TEXTURE_BED			"assets/texture/start_bed.png"
+#define TEXTURE_DEBUG				"assets/texture/debug_font_32x64.png"
+#define TEXTURE_BG					"assets/texture/background_only.png"
+#define TEXTURE_PLAYER				"assets/texture/player.png"
+#define TEXTURE_ROBOT				"assets/texture/wurenji01.png"
+
+#define TEXTURE_OBJECT_BED			"assets/texture/start_bed.png"
+#define TEXTURE_OBJECT_FLOOR		"assets/texture/2floor.png"
+#define TEXTURE_OBJECT_LADDER		"assets/texture/ladder.png"
