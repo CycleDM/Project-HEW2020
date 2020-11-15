286db13fa1eb30144e0c9503a05e51192d2ce3f8
CycleDM <cycledm0508@gmail.com>
2020-11-16 04:31:39 +0900

    Update config.h

--- Project-HEW2020/config.h
+++ Project-HEW2020/config.h
@@ -13,15 +13,21 @@
 #define SCREEN_WIDTH (1280)
 #define SCREEN_HEIGHT (720)
 
+// 重力システム
+#define GAME_GRAVITY	(9.8f)
+
 // ゲームオブジェクトの最大数
 #define SINGLE_SCENE_OBJECT_MAX		(128)
-#define SINGLE_SCENE_OVERLAY_MAX	(4)
+#define SINGLE_SCENE_OVERLAY_MAX	(8)
 
 // テクスチャーのファイル名
 #define TEXTURE_DEBUG				"assets/texture/debug_font_32x64.png"
 #define TEXTURE_BG					"assets/texture/background_only.png"
 #define TEXTURE_BG_OVERLAY			"assets/texture/background_overlay.png"
+
 #define TEXTURE_PLAYER				"assets/texture/player.png"
+#define TEXTURE_PLAYER_CLIMBING		"assets/texture/player_climbing.png"
+
 #define TEXTURE_ROBOT				"assets/texture/wurenji01.png"
 
 #define TEXTURE_OBJ_BED				"assets/texture/start_bed.png"
